c52c7f874ed237e65708bcbbdfb792b113fc0c90
jubayerAshrafMahin <mahinashraf991@gmail.com>
2024-12-26 11:48:46 +0600

    added circular form1

---
+++ app/src/main/java/com/example/rent_rover/CircularFormActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rent_rover
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CircularFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_form_circular);
+    }
+}
