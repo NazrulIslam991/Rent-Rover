c19c86c990e06ae8bf1017f593b05531dd21f400
Md Nazrul Islam Nayon <nazrulislamnayon991@gmail.com>
2025-01-31 10:36:50 +0600

    Mahin (#30)
    * .idea file updated
    
    * added profile activity and part of filter activity
    
    ---------
    
    Co-authored-by: jubayerAshrafMahin <mahinashraf991@gmail.com>

--- app/src/main/java/com/example/rent_rover/MenuFragment.kt
+++ app/src/main/java/com/example/rent_rover/MenuFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +35,7 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        return inflater.inflate(R.layout.activity_profile, container, false)
     }
 
     companion object {
